3296d98ae52cafcb1f002839fd8535a97546a5c9
Ryan <changche918@gmail.com>
2026-01-18 22:50:04 +0800

    20260118

--- test1.py
+++ test1.py
@@ -1,7 +1,7 @@
-# 上升部分
-for i in range(5):
-    star = '*' * (2 * i + 1)
-    print(star.center(9))
+# # 上升部分
+# for i in range(5):
+#     star = '*' * (2 * i + 1)
+#     print(star.center(9))
 
 # 下降部分
 for i in range(4, 0, -1):
