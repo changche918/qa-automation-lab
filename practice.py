2265537236b1d21d225f9db9124f991d7c3b0432
Ryan <changche918@gmail.com>
2026-01-26 20:26:21 +0800

    20260126

---
+++ practice.py
@@ -0,0 +1,27 @@
+# n = float(input("請輸入一個數字: "))
+# if n > 0:
+#     print("positive")
+# elif n < 0:
+#     print("negative")
+# else:
+#     print("zero")
+
+
+
+# score = float(input("請輸入分數: "))
+# if score >= 90:
+#     print("A")
+# elif score >= 80:
+#     print("B")
+# elif score >= 70:
+#     print("C")
+# else:
+#     print("F")
+
+number = float(input("請輸入一個數字: "))
+if number % 2 == 0 and number > 10:
+    print("Even and > 10")
+elif number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
