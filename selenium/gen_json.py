b98d54cf28015a72fd03e7de5e3ff9b598d9d129
Ryan <changche918@gmail.com>
2026-02-13 15:26:33 +0800

    20260213

--- selenium/gen_json.py
+++ selenium/gen_json.py
@@ -1,25 +1,17 @@
 import json
 
 class DataSaver:
-
-    def __init__(self, title="適合你的好工作"):
+    def __init__(self, title):
         self.title = title
-
-    def save(self, elements, filename):
-        data = {
-            self.title: {
-                el.text: el.get_attribute("href")
-                for el in elements
-            }
-        }
-
+    # def json_dict(self, data):
+    #     self.data = data
+    def save(self, filename):
         with open(filename, "w", encoding="utf-8") as file:
-            json.dump(data, file, indent=2, ensure_ascii=False)
-
-        print(f"{filename} 儲存完成")
-
-
-# 這一行會讓 json 再包一層
-# data = {"適合你的好工作": {job_list.text: job_list.get_attribute("href")for job_list in tabs}}
-# with open("selenium\data_1.json", "w", encoding="utf-8") as file:
-#     json.dump(data, file, indent=2, ensure_ascii=False)
+            json.dump(self, file, indent=2, ensure_ascii=False)
+            print(f"{filename} 儲存完成")
+    def load(self, filename):
+        pass
+    def update(self, filename):
+        pass
+    def delete(self, filename):
+        pass
---
+++ selenium/practice_seleuium_0213.py
@@ -0,0 +1,102 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import gen_json
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+"""
+TODO: 
+1. 到https://www.104.com.tw/網站 -> 找到五個區塊並逐一命名, 最少成印出文字+link(href)
+    -> optimize hint: 能拆class/function | 能把區塊拆分更細 |
+2. 存成txt檔案
+    -> optimize hint: json格式的txt
+"""
+driver = webdriver.Chrome()
+driver.get("https://www.104.com.tw/")
+
+# 登入看專屬職缺
+login_to_see_elem = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, "//*[text()='登入看專屬職缺 ']")))
+login_to_see_elem_href = login_to_see_elem.get_attribute("href")
+# text_1 = driver.find_elements(By.CSS_SELECTOR, ".jb-link.jb-link-blue.h3")[0].text
+# text_1_href = driver.find_elements(By.CSS_SELECTOR, ".jb-link.jb-link-blue.h3")[0].get_attribute("href")
+# print(f"抓到的文字是 : {text_1} ,連結是 {text_1_href}")
+
+# 地區找工作
+area_to_find_elem = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, "//*[text()='地區找工作']")))
+area_to_find_elem_href = driver.find_element(By.XPATH, "//*[text()='地區找工作']").get_attribute("href")
+# text_2 = driver.find_elements(By.CSS_SELECTOR, ".category-item__text__name.d-block.h4")[0].text
+# text_2_href = driver.find_elements(By.CSS_SELECTOR, ".category-item__text__name.d-block.h4")[0].get_attribute("href")
+# print(f"抓到的文字是 : {text_2} ,連結是 {text_2_href}")
+
+# 上市櫃
+listing_and_OTC_elem = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, "//*[text()='上市櫃']")))
+listing_and_OTC_elem_href = driver.find_element(By.XPATH, "//*[text()='上市櫃']").get_attribute("href")
+# text_3 = driver.find_elements(By.CSS_SELECTOR, ".router-link-active.router-link-exact-active")[1].text
+# text_3_href = driver.find_elements(By.CSS_SELECTOR, ".router-link-active.router-link-exact-active")[1].get_attribute("href")
+# print(f"抓到的文字是 : {text_3} ,連結是 {text_3_href}")
+
+# 前往職涯診所
+go_to_job_clinic_elem = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, "//*[text()='前往職涯診所']")))
+go_to_job_clinic_elem_href = driver.find_element(By.XPATH, "//*[text()='前往職涯診所']").get_attribute("href")
+# text_4 = driver.find_elements(By.CSS_SELECTOR, ".btn.btn-sm.btn-outline-primary.mt-2.mt-md-7")[0].text
+# text_4_href = driver.find_elements(By.CSS_SELECTOR, ".btn.btn-sm.btn-outline-primary.mt-2.mt-md-7")[0].get_attribute("href")
+# print(f"抓到的文字是 : {text_4} ,連結是 {text_4_href}")
+
+# 檢視職業適合度
+view_job_fit_elem = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, "//*[text()='檢視職業適合度']")))
+view_job_fit_elem_href = driver.find_element(By.XPATH, "//*[text()='檢視職業適合度']").get_attribute("href")
+# text_5 = driver.find_elements(By.CSS_SELECTOR, ".btn.btn-sm.btn-outline-primary.mt-2.mt-md-7")[1].text
+# text_5_href = driver.find_elements(By.CSS_SELECTOR, ".btn.btn-sm.btn-outline-primary.mt-2.mt-md-7")[1].get_attribute("href")
+# print(f"抓到的文字是 : {text_5} ,連結是 {text_5_href}")
+
+job_type_dict = {
+    login_to_see_elem.text: login_to_see_elem_href,
+    area_to_find_elem.text: area_to_find_elem_href,
+    listing_and_OTC_elem.text: listing_and_OTC_elem_href,
+    go_to_job_clinic_elem.text: go_to_job_clinic_elem_href,
+    view_job_fit_elem.text: view_job_fit_elem_href
+}
+
+if job_type_dict:
+    for job_name, job_link in job_type_dict.items():
+        # job_type_dict[el.text] = el.get_attribute("href")
+        print(f"名稱 : {job_name} , 連結 : {job_link}")
+        # 標籤名 : 地區找工作 , 連結 : https://a.com
+"""   
+for key, value in job_type_dict.items():
+print(f"標籤名 : {key} , 連結 : {value}")
+"""
+
+# # data = {text_1: text_1_href, text_2: text_2_href, text_3: text_3_href, text_4: text_4_href, text_5: text_5_href}
+
+gen_json_json = gen_json.DataSaver(title="適合你的好工作")
+gen_json_json.save("selenium\data.json")
+
+driver.quit()
+
+
+
+"""
+TODO: 
+1. 正常版本會包含前面的code邏輯 
+2. 將第一版改為優化版
+3. debug "上市櫃" (或換寫法)
+4. (可讀性)
+5. 物件建立方式
+6. for迴圈盡量先少用一行用法 下方為sample
+  `  data = {}
+    for el in elements:
+        `data[el.text] = el.get_attribute("href")
+7. 單一職責 (思考功能拆分 盡量要單位小 **但不能過小**) 
+8. DataSaver 還有什麼功能可以先預寫3個 裡面可以pass 有空的話寫簡單的內容
+9. 嘗試不用XPATH抓 data-gtm-index 這些(可讀性)
+10. 嘗試直接抓文字, 類似: text()="中高齡"
+"""
+
+
