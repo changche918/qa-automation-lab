8f819ff1798492ed41498decd7e3a5bfa2aa435a
Ryan <changche918@gmail.com>
2026-02-14 00:41:49 +0800

    20260214

--- selenium/gen_json.py
+++ selenium/gen_json.py
@@ -3,11 +3,11 @@ import json
 class DataSaver:
     def __init__(self, title):
         self.title = title
-    # def json_dict(self, data):
-    #     self.data = data
-    def save(self, filename):
+    def elem_dict(self, data):
+        self.data = data
+    def save(self, data, filename):
         with open(filename, "w", encoding="utf-8") as file:
-            json.dump(self, file, indent=2, ensure_ascii=False)
+            json.dump(data, file, indent = 4, ensure_ascii=False)
             print(f"{filename} 儲存完成")
     def load(self, filename):
         pass
